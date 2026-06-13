c05dd6a79293391690383c7e092301c4c03c548a
YeisonMC <yeisonmacalupu01@gmail.com>
2026-06-13 08:27:28 -0500

    add components

--- src/api/musicApi.js
+++ src/api/musicApi.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API_URL =
-  import.meta.env.VITE_API_URL ??
-  "https://recomendador-musical-backend.onrender.com/api";
+const API_URL = "https://recomendador-musical-backend.onrender.com/api";
 // const API_URL = import.meta.env.VITE_API_URL ?? "http://127.0.0.1:8000/api";
 
 const musicApi = axios.create({
